350a0a9ee2ae7744892200ba11d5ad48735bebf0
Aro Ruby <aros@rambler.ru>
2016-11-29 23:45:38 +0300

    Add page new

--- app.rb
+++ app.rb
@@ -5,3 +5,6 @@ require 'sqlite3'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra\">Original</a> pattern has been modifiled for Rubi School"	
 end
+get '/new' do
+   erb "Hello World"
+end
