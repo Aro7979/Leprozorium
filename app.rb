e29f2e2661551d9fc3d475a275dbba650016acab
Aro Ruby <aros@rambler.ru>
2016-11-30 11:13:38 +0300

    init database

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+def init_db
+@db=SQLite3::Database.new'leprozorium.db'
+@db.results_as_hash=true
+end
+
+before do
+	init_db
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra\">Original</a> pattern has been modifiled for Rubi School"	
 end
@@ -9,6 +18,7 @@ get '/new' do
    erb :new
 end
 post '/new' do
+	
 content= params[:content]
 erb"You typed #{content}"
 end
